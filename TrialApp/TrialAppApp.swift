6adb8bee45f941b2041129b85937d602b63f8ab2
Emirhan Kartal <emirhan.kartal@twiser.com>
2024-08-21 14:12:22 +0300

    Initial Commit

---
+++ TrialApp/TrialAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrialAppApp.swift
+//  TrialApp
+//
+//  Created by Emiran Kartal on 21.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct TrialAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TrialAppUITests/TrialAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TrialAppUITestsLaunchTests.swift
+//  TrialAppUITests
+//
+//  Created by Emiran Kartal on 21.08.2024.
+//
+
+import XCTest
+
+final class TrialAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
